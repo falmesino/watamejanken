0a8734249e08ae7de89aaef6c2532b8ca3b44e14
Falmesino Abdul Hamid <falmesino@gmail.com>
2020-02-26 18:37:07 +0700

    Add the files

---
+++ assets/js/main.js
@@ -0,0 +1,173 @@
+/**
+ * Tsunomaki Janken
+ * Created by @falmesino
+ */
+
+var userHand = '';
+var watameHand = '';
+var score = 0;
+var winStreak = 0;
+var loseStreak = 0;
+
+$(function(){
+
+    console.log('Document is ready, my Lord.');
+
+    $(document).on('click', '#btnPlay', function(e){
+        e.preventDefault();
+        console.log('Play button has been clicked!');
+        gameScreen();
+        return false;
+    });
+
+});
+
+function resetChoice() {
+    $('.wj-hands--player').stop().removeClass('ready').removeClass('locked');
+    $('.wj-hands--player .wj-hands__item.selected').stop().removeClass('selected');
+    $('.wj-hands--watame img.active').stop().removeClass('active');
+    $('#btnReplay').stop().addClass('d-none');
+}
+
+function lockChoice(target) {
+    $('.wj-hands--player').stop().addClass('locked');
+    $('.wj-hands--player .wj-hands__item.selected').stop().removeClass('selected');
+    target.stop().addClass('selected');
+}
+
+function ctaScreen() {
+    $('.wj-scene.active').stop().removeClass('active');
+    $('.wj-scene--cta').stop().addClass('active');
+}
+
+function getWatameHand() {
+    let watameHands = ['Rock', 'Paper', 'Scissor'];
+    let output = watameHands[Math.floor(Math.random() * watameHands.length)];
+    // console.log('Watame choose : ' + output);
+    return output;
+}
+
+function gameScreen() {
+
+    resetChoice();
+
+    $('.wj-scene.active').stop().removeClass('active');
+    $('.wj-scene--main').stop().addClass('active');
+
+    let watameRandom = getWatameHand();
+
+    var videoPlayer = new Plyr('#videoPlayer', {
+        debug: false,
+        autoplay: false,
+        resetOnEnd: true,
+        controls: [],
+        settings: [],
+        volume: 1,
+        clickToPlay: false,
+        disableContextMenu: true,
+        hideControls: true,
+        keyboard: {
+            focused: false,
+            seek: false
+        },
+        quality: {
+            default: 1080,
+            options: [720, 1080]
+        },
+        youtube: {
+            noCookie: false,
+            rel: 0,
+            showinfo: 0,
+            iv_load_policy: 3,
+            modestbranding: 1,
+            diasblekb: 1,
+            controls: 0
+        }
+    });
+
+    $(document).on('click', '#btnHandRock', function(e){
+        e.preventDefault();
+        lockChoice($(this));
+        userHand = 'rock';
+        videoPlayer.play();
+        return false;
+    });
+
+    $(document).on('click', '#btnHandPaper', function(e){
+        e.preventDefault();
+        lockChoice($(this));
+        userHand = 'paper';
+        videoPlayer.play();
+        return false;
+    });
+
+    $(document).on('click', '#btnHandScissor', function(e){
+        e.preventDefault();
+        lockChoice($(this));
+        userHand = 'scissor';
+        videoPlayer.play();
+        return false;
+    });
+
+    $(document).on('click', '#btnTimer', function(e){
+        e.preventDefault();
+        let currentTime = videoPlayer.media.currentTime;
+        console.log(currentTime);
+        return false;
+    });
+
+    $(document).on('click', '#btnReset', function(e){
+        e.preventDefault();
+        resetChoice();
+        $('.wj-hands--player').stop().addClass('ready');
+        $('.wj-hands--watame img.active').stop().removeClass('active');
+        watameRandom = getWatameHand();
+        videoPlayer.stop();
+        return false;
+    });
+
+    $(document).on('click', '#btnReplay', function(e){
+        e.preventDefault();
+        resetChoice();
+        $('.wj-hands--player').stop().addClass('ready');
+        watameRandom = getWatameHand();
+        videoPlayer.stop();
+        return false;
+    });
+
+    $(document).on('click', '#btnPlayTest', function(e){
+        e.preventDefault();
+        videoPlayer.play();
+        return false;
+    });
+
+    videoPlayer.on('ready', function(event){
+        $('.wj-hands--player').stop().addClass('ready');
+    });
+
+    videoPlayer.on('ended', function(){
+        $('#btnReplay').stop().fadeIn('fast', function(){
+            $(this).stop().removeClass('d-none');
+        });
+    });
+
+    videoPlayer.on('timeupdate', function(event){
+        let currentTime = event.detail.plyr.media.currentTime;
+        let targetTime = 13.50;
+        let endTime = 20.00;
+
+        if(currentTime >= targetTime) {
+            $('.wj-hands--watame #watame' + watameRandom).stop().addClass('active');
+        }
+
+        if(currentTime >= endTime) {
+            videoPlayer.stop();
+
+            $('#btnReplay.d-none').stop().fadeIn('fast', function(){
+                $(this).stop().removeClass('d-none');
+            });
+        }
+
+    });
+
+}
